4762bee01d5b6c338e3dbbc0fbd93e4c93d4494d
TeHe4K <hryblackwii@gmail.com>
2025-10-20 18:39:56 +0500

    Разработанный класс «Human» Коневский

---
+++ Classes/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr10.Classes
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public Human(string Name, string Img) 
+        {
+            this.Name = Name;
+            this.Img = Img;
+        }
+
+        public abstract void Speak(Label Phrase);
+    }
+}
